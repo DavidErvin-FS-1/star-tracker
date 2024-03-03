8f5cd9d58a235792bf72726caa5810dc206b14de
David Ervin <dnervin@student.fullsail.edu>
2024-03-03 13:58:26 -0500

    Update view engine and render home page with template

--- index.js
+++ index.js
@@ -4,24 +4,25 @@ const express = require(`express`)
 // Create a new Express instance called "app"
 const app = express()
 
-app.use(express.json())
+// app.use(express.json())
 
-app.configure(function () {
-  app.set('views', __dirname + '/views')
-  app.set('view engine', 'twig')
+app.set('views', __dirname + '/templates')
+app.set('view engine', 'twig')
 
-  // This section is optional and can be used to configure twig.
-  app.set('twig options', {
-    strict_variables: false
-  })
-})
+// This section is optional and can be used to configure twig.
+// app.set('twig options', {
+//   strict_variables: false
+// })
 
 // Load in our RESTful routers
 const routers = require('./routers/index.js')
 
 // Home page welcome middleware
 app.get('/', (req, res) => {
-  res.status(200).send('Welcome to Star Tracker Library')
+  // res.status(200).send('Welcome to Star Tracker Library')
+  res.status(200).render('views/Default/home.html.twig', {
+    title: 'Welcome to Star Tracker Library'
+  })
 })
 
 // Register our RESTful routers with our "app"
